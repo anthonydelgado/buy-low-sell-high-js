236d9f113d33899f582f6f2a62425f3ec6bdd78f
Anthony Delgado <me@anthonydelgado.me>
2016-09-27 19:40:09 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+var  arrayOfStockPrices =  [1.42, 1.32, 1.45, 1.20, 1.34, 1.74, 1.10, 1.89, 1.42, 1.90] ; 
+
+Function showBiggestProfit(arrayOfStockPrices){
+
+For each (arrayOfStockPrices) {
+
+StockPrices
+
+For each (arrayOfStockPrices) {
+
+StockPrices - StockPrices 
+
+}
+
+}
+
+
+}
